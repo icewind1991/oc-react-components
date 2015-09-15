05c5b8b27db19a230d8a05ac08522e35defcdc2c
Robin Appelman <icewind@owncloud.com>
2015-09-15 21:16:38 +0200

    Add settings component for sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -18,7 +18,7 @@ export class Entry extends Component {
 	render () {
 		var className = this.props.icon ? 'icon-' + this.props.icon : '';
 		return (
-			<li>
+			<li className={this.props.className||''}>
 				<a className={className} onClick={this.props.onClick||(()=>{})}>
 					{this.props.children}
 				</a>
@@ -31,6 +31,38 @@ export class Separator extends Component {
 	render () {
 		return (
 			<li className="app-navigation-separator"/>
+		);
+	}
+}
+
+export class Settings extends Component {
+	state = {
+		show: false
+	};
+
+	toggle = () => {
+		const show = !this.state.show;
+		this.setState({show});
+	};
+
+	render () {
+		const title = (this.props.title) ? this.props.title : 'Settings';
+		const style = {
+			display: (this.state.show) ? 'none' : 'block'
+		};
+
+		return (
+			<div id="app-settings">
+				<div id="app-settings-header">
+					<button className="settings-button"
+							onClick={this.toggle}>{title}
+					</button>
+				</div>
+				<div
+					style={style}>
+					{this.props.children}
+				</div>
+			</div>
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {SideBar, Entry, Separator} from './SideBar.js';
+import {SideBar, Entry, Separator, Settings} from './SideBar.js';
 import {App, Content, ControlBar} from './App.js';
 
-export {SideBar, Entry, Separator, App, Content, ControlBar};
+export {SideBar, Entry, Separator, App, Content, ControlBar, Settings};
